33f1b171a7b1298b50042c6594e1549660f9c895
Cillian Barron <barroncillian@gmail.com>
2026-03-16 15:09:47 +0000

    chore(email): remove package implementation for tdd replay

--- packages/email/src/package-surface.test.ts
+++
@@ -1,8 +0,0 @@
-import assert from "node:assert/strict"
-import test from "node:test"
-
-import * as emailPackage from "./index.ts"
-
-test("root export surface does not expose provider-specific errors", () => {
-  assert.equal("EmailTransportError" in emailPackage, false)
-})
